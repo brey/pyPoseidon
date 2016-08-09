1365e274df02055ec4e2e3fe9e150fee0c062371
brey <breyiannis@gmail.com>
2016-08-09 16:12:57 +0200

    consolidate dem function to work with all bathymetries

--- DELFT3D/dem.py
+++ DELFT3D/dem.py
@@ -6,7 +6,7 @@ import scipy.interpolate
 import sys
 import grid
 
-PATH='../BATHYMETRY/'
+#PATH='../BATHYMETRY/'
 
 def pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title=None):
 
@@ -24,20 +24,62 @@ def pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title=None):
 #plt.savefig('topo.png', bbox_inches='tight')
  plt.show(block=False)
 
-def readsrtm(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=False):
- file=PATH+'GLOBAL/topo30.grd'
+def readem(minlat,maxlat,minlon,maxlon,filename,grid_x=None,grid_y=None,plot=False,interpolate=False):
+#file=PATH+'GLOBAL/GEBCO_2014_2D.nc'
 # open NetCDF data in 
- nc = netCDF4.Dataset(file)
+ nc = netCDF4.Dataset(filename)
  ncv = nc.variables
 #print ncv.keys()
+ nv=ncv.keys()
+ if nv[0] == 'elevation' :
+          n3,n2,n1=nv[0],nv[1],nv[2]
+ else:
+          n1,n2,n3=nv[0],nv[1],nv[2]
 
- lon = ncv['lon'][:]
- lat = ncv['lat'][:]
+ lon = ncv[n1][:]
+ lat = ncv[n2][:]
 
+ if 'topo30.grd' not in filename:
 
-# Shift longitude if neccessary
+  if maxlon > 180:
 
- if minlon < 0:
+    lon=lon+180.
+
+    i1=np.abs(lon-minlon).argmin()
+    if lon[i1] > minlon: i1=i1-1
+    i2=np.abs(lon-maxlon).argmin()
+    if lon[i2] < maxlon: i2=i2+1
+
+    j1=np.abs(lat-minlat).argmin()
+    if lat[j1] > minlat: j1=j1-1
+    j2=np.abs(lat-maxlat).argmin()
+    if lat[j2] < maxlat: j2=j2+1
+
+    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
+
+    zlon=lon.shape[0]
+
+    topo = ncv[n3][j1:j2,zlon/2+i1:]
+    topo = np.hstack([topo,ncv[n3][j1:j2,:i2-zlon/2]])
+
+  else:
+
+    i1=np.abs(lon-minlon).argmin()
+    if lon[i1] > minlon: i1=i1-1
+    i2=np.abs(lon-maxlon).argmin()
+    if lon[i2] < maxlon: i2=i2+1
+
+    j1=np.abs(lat-minlat).argmin()
+    if lat[j1] > minlat: j1=j1-1
+    j2=np.abs(lat-maxlat).argmin()
+    if lat[j2] < maxlat: j2=j2+1
+
+    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
+    topo = ncv[n3][j1:j2,i1:i2]
+
+ else:
+
+  if minlon < 0:
 
     lon=lon-180.
 
@@ -55,10 +97,10 @@ def readsrtm(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=Fal
 
     zlon=lon.shape[0]
 
-    topo = ncv['z'][j1:j2,zlon/2+i1:]
-    topo = np.hstack([topo,ncv['z'][j1:j2,:i2-zlon/2]])
+    topo = ncv[n3][j1:j2,zlon/2+i1:]
+    topo = np.hstack([topo,ncv[n3][j1:j2,:i2-zlon/2]])
 
- else:
+  else:
 
     i1=np.abs(lon-minlon).argmin()
     if lon[i1] > minlon: i1=i1-1
@@ -71,9 +113,10 @@ def readsrtm(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=Fal
     if lat[j2] < maxlat: j2=j2+1
 
     lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
-    topo = ncv['z'][j1:j2,i1:i2]
+    topo = ncv[n3][j1:j2,i1:i2]
 
- pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title='SRTM30 - READ')
+ title=filename.split('/')[-1]
+ if plot : pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title=title +'- READ')
 
  if interpolate :
 # interpolate on the given grid
@@ -88,144 +131,11 @@ def readsrtm(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=Fal
 
   itopo=np.array(itopo)
   itopo=itopo.reshape(grid_x.shape)
-  pltm(minlat,maxlat,minlon,maxlon,grid_x,grid_y,itopo,title='SRTM30 interpolated')
+  if plot : pltm(minlat,maxlat,minlon,maxlon,grid_x,grid_y,itopo,title=title+'- interpolated')
   return itopo
  else:
   return lons,lats,topo
 
-
-def readgebco14(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=False):
- file=PATH+'GLOBAL/GEBCO_2014_2D.nc'
-# open NetCDF data in 
- nc = netCDF4.Dataset(file)
- ncv = nc.variables
-#print ncv.keys()
-
- lon = ncv['lon'][:]
- lat = ncv['lat'][:]
-
- if maxlon > 180:
-
-    lon=lon+180.
-
-    i1=np.abs(lon-minlon).argmin()
-    if lon[i1] > minlon: i1=i1-1
-    i2=np.abs(lon-maxlon).argmin()
-    if lon[i2] < maxlon: i2=i2+1
-
-    j1=np.abs(lat-minlat).argmin()
-    if lat[j1] > minlat: j1=j1-1
-    j2=np.abs(lat-maxlat).argmin()
-    if lat[j2] < maxlat: j2=j2+1
-
-    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
-
-    zlon=lon.shape[0]
-
-    topo = ncv['elevation'][j1:j2,zlon/2+i1:]
-    topo = np.hstack([topo,ncv['elevation'][j1:j2,:i2-zlon/2]])
-
- else:
-
-    i1=np.abs(lon-minlon).argmin()
-    if lon[i1] > minlon: i1=i1-1
-    i2=np.abs(lon-maxlon).argmin()
-    if lon[i2] < maxlon: i2=i2+1
-
-    j1=np.abs(lat-minlat).argmin()
-    if lat[j1] > minlat: j1=j1-1
-    j2=np.abs(lat-maxlat).argmin()
-    if lat[j2] < maxlat: j2=j2+1
-
-    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
-    topo = ncv['elevation'][j1:j2,i1:i2]
-
- pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title='GEBCO 2014 - READ')
-
- if interpolate :
-# interpolate on the given grid
-  #flip on lat to make it increasing for RectBivariateSpline
-  ilon=lons[0,:]
-  ilat=lats[:,0]
-  sol=scipy.interpolate.RectBivariateSpline(ilon,ilat,topo.T)
-
-  itopo=[]
-  for x,y in zip(grid_x.ravel(),grid_y.ravel()):
-      itopo.append(sol(x,y).ravel()[0])
-
-  itopo=np.array(itopo)
-  itopo=itopo.reshape(grid_x.shape)
-  pltm(minlat,maxlat,minlon,maxlon,grid_x,grid_y,itopo,title='GEBCO 2014 - interpolated')
-  return itopo
- else:
-  return lons,lats,topo
-
-
-def read_mod_gebco(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpolate=False):
- file=PATH+'GLOBAL/gebco30.nc'
-# open NetCDF data in 
- nc = netCDF4.Dataset(file)
- ncv = nc.variables
-#print ncv.keys()
-
- lon = ncv['x'][:]
- lat = ncv['y'][:]
-
- if maxlon > 180:
-
-    lon=lon+180.
-
-    i1=np.abs(lon-minlon).argmin()
-    if lon[i1] > minlon: i1=i1-1
-    i2=np.abs(lon-maxlon).argmin()
-    if lon[i2] < maxlon: i2=i2+1
-
-    j1=np.abs(lat-minlat).argmin()
-    if lat[j1] > minlat: j1=j1-1
-    j2=np.abs(lat-maxlat).argmin()
-    if lat[j2] < maxlat: j2=j2+1
-
-    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
-
-    zlon=lon.shape[0]
-
-    topo = ncv['z'][j1:j2,zlon/2+i1:]
-    topo = np.hstack([topo,ncv['z'][j1:j2,:i2-zlon/2]])
-
- else:
-
-    i1=np.abs(lon-minlon).argmin()
-    if lon[i1] > minlon: i1=i1-1
-    i2=np.abs(lon-maxlon).argmin()
-    if lon[i2] < maxlon: i2=i2+1
-
-    j1=np.abs(lat-minlat).argmin()
-    if lat[j1] > minlat: j1=j1-1
-    j2=np.abs(lat-maxlat).argmin()
-    if lat[j2] < maxlat: j2=j2+1
-
-    lons, lats = np.meshgrid(lon[i1:i2],lat[j1:j2])
-    topo = ncv['z'][j1:j2,i1:i2]
-
- pltm(minlat,maxlat,minlon,maxlon,lons,lats,topo,title='MODIFIED GEBCO - READ')
-
- if interpolate :
-# interpolate on the given grid
-  #flip on lat to make it increasing for RectBivariateSpline
-  ilon=lons[0,:]
-  ilat=lats[:,0]
-  sol=scipy.interpolate.RectBivariateSpline(ilon,ilat,topo.T)
-
-  itopo=[]
-  for x,y in zip(grid_x.ravel(),grid_y.ravel()):
-      itopo.append(sol(x,y).ravel()[0])
-
-  itopo=np.array(itopo)
-  itopo=itopo.reshape(grid_x.shape)
-  pltm(minlat,maxlat,minlon,maxlon,grid_x,grid_y,itopo,title='MODIFIED GEBCO - interpolated')
-  return itopo
- else:
-  return lons,lats,topo
 
 
 #############################################################################################################
@@ -235,21 +145,20 @@ def read_mod_gebco(minlat,maxlat,minlon,maxlon,grid_x=None,grid_y=None,interpola
 # Definine the domain of interest
 if __name__ == "__main__":
  try:
-    minlat=sys.argv[1]
-    maxlat=sys.argv[2]
-    minlon=sys.argv[3]
-    maxlon=sys.argv[4]
-    stride=sys.argv[5]
+    minlon=sys.argv[1]
+    maxlon=sys.argv[2]
+    minlat=sys.argv[3]
+    maxlat=sys.argv[4]
+    filename=sys.argv[5]
  except:
-    minlat = 28.
-    maxlat = 48.
+    minlat = 28.5
+    maxlat = 47.5
     minlon = -5.5
-    maxlon = 47.5
+    maxlon = 43.
 
- l1,l2,ba = readsrtm(float(minlat),float(maxlat),float(minlon),float(maxlon))
- c1,c2,bg = readgebco(float(minlat),float(maxlat),float(minlon),float(maxlon))
- m1,m2,bm = read_mod_gebco(float(minlat),float(maxlat),float(minlon),float(maxlon))
+ l1,l2,ba = readem(float(minlat),float(maxlat),float(minlon),float(maxlon),filename)
+ m1,m2,bg = readem(float(minlat),float(maxlat),float(minlon),float(maxlon),'../BATHYMETRY/GLOBAL/topo30.grd')
 
  topo=ba-bg
- pltm(minlat,maxlat,minlon,maxlon,m1,m2,topo,title='SRTM-GEBCO (DIFF)')
+ pltm(float(minlat),float(maxlat),float(minlon),float(maxlon),m1,m2,topo,title='GEBCO_Deltares-GEBCO (DIFF)')
  plt.show(block=False)
