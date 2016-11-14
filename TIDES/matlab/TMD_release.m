a09d2dce0879bafb6afd81cb66d0910012c0babf
brey <breyiannis@gmail.com>
2016-11-14 14:13:38 +0100

    scrap TIDES OTPS from version control

--- TIDES/matlab/TMD_release.m
+++
@@ -1,1 +0,0 @@
-fprintf('TMD release 2.02: 21 July 2010\n');
--- TIDES/matlab/TMD_timeOnOff.m
+++
@@ -1,8 +0,0 @@
-% 
-function []=TMD_timeOnOff(uTime,uT,action);
-set(uTime,'Enable',action);
-nt=length(uT);
-for it=1:nt,
- set(uT(it),'Enable',action);
-end
-return
