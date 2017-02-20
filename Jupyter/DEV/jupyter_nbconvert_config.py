f533b472220a53a89d116c8f758cb57afeddc6a8
brey <breyiannis@gmail.com>
2017-02-20 11:59:18 +0100

    cleanup

--- Jupyter/DEV/jupyter_nbconvert_config.py
+++
@@ -1,4 +0,0 @@
-c = get_config()
-#c.Exporter.preprocessors = [ 'bibpreprocessor.BibTexPreprocessor', 'pymdpreprocessor.PyMarkdownPreprocessor' ]
-c.Exporter.preprocessors = [ 'pre_markdown.py.MarkdownPreprocessor' ]
-c.Exporter.template_file = 'revtex_nocode.tplx'
