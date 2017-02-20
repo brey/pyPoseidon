b53999907ae32153d137a34950678a9743e5612d
brey <breyiannis@gmail.com>
2017-02-20 11:35:02 +0100

    update bnd file

--- DELFT3D/bnd.py
+++ DELFT3D/bnd.py
@@ -17,7 +17,8 @@ basename='eur'
 grd=Grid.fromfile(RPATH+basename+'.grd')
 dlat=grd.y[1,0]-grd.y[0,0]
 dlon=grd.x[0,1]-grd.x[0,0]
-
+lons=grd.x[0,:]
+lats=grd.y[:,0]
 
 ba=Dep.read(RPATH+basename+'.dep',grd.shape)
 
@@ -34,58 +35,75 @@ tidal_c=[''.join(k).upper().strip() for k in tidal_c]
 amp=dmed['tidal_amplitude_h']
 ph=dmed['tidal_phase_h']
 
-#Identify WEST boundary 
+#adjust lon according to the grid window
+if lons[0]<0 :
+    ii=np.abs(lon-180.).argmin()
+    topo = lon[ii:]-360.
+    lon = np.hstack([topo,lon[:ii]])
+    topo = amp[ii:,:,:]
+    amp = np.vstack([topo,amp[:ii,:,:]])
+    topo = ph[ii:,:,:]
+    ph = np.vstack([topo,ph[:ii,:,:]])
 
-west=[]
-for j in range(grd.y.shape[0]):
-  if ba.val[j,0] > 0. : west.append(j)
-
-# split into branches    
-s0=[x for x in west if x-1 not in west]
-s1=[x for x in west if x+1 not in west]
 
 # strings to be used 
 le=['A','B']
 
+n=10  # points in chunks of the boundary
+
+dic={'West':ba.val[:-1,0],'East':ba.val[:-1,-2],'South':ba.val[0,:-1],'North':ba.val[-2,:-1]}
+idic={'West':[0,-99],'East':[lons.shape[0],-99],'South':[-99,0],'North':[-99,lats.shape[0]]}
+
+#initiate output files
+f = open(RPATH+basename+'.bnd', 'w')
+f.close()
+f = open(RPATH+basename+'.bca', 'w')
+f.close()
+
+def getboundary(bound,llon,llat):
+
+#Identify WEST boundary 
+   bv = dic[bound] 
+
+   v=np.isfinite(bv).nonzero()
+   branches = [list(x) for x in v]
 
 #iterate over all branches
-for g,h in zip(s0,s1):
+   for b in branches:
 
-  swest = west[g-1:h+1] if g not in [0,1] else west[1:h+1]
+    s = b if b[0] not in [0,1] else b[1:]
 
 # make chunks of n points if range is large
-  n=10
-  chunks = [ swest[i:i+n] for i in xrange(0, len(swest),n if n < len(swest) else len(swest)) ]
-  if len(chunks[-1]) < 3 : 
-    chunks[-2]=chunks[-2]+chunks[-1] # merge the last chunk if too small
-    chunks = chunks[:-1] # eliminate the last chunk
+    chunks = [ s[i:i+n] for i in xrange(0, len(s),n if n < len(s) else len(s)) ]
+    if len(chunks[-1]) < 3 : 
+      chunks[-2]=chunks[-2]+chunks[-1] # merge the last chunk if too small
+      chunks = chunks[:-1] # eliminate the last chunk
 
 #write bnd file
-  with open(RPATH+basename+'.bnd', 'wa') as f:
-
-   for k in chunks:
-     f.write('West{}                Z A     1    {}     1    {}   0.0000000e+00 West{}A West{}B\n'.format(k[0],k[0]+1,k[-1]+1,k[0],k[0])) # fortran index ??
+    with open(RPATH+basename+'.bnd', 'a') as f:
+      for ch in chunks:
+        k1 = [ch[0] if x==-99 else x for x in idic[bound]] 
+        k2 = [ch[-1] if x==-99 else x for x in idic[bound]] 
+        f.write('{}{}                Z A     {}    {}     {}    {}   0.0000000e+00 {}{}A {}{}B\n'.format(bound,ch[0],k1[0]+1,k1[1]+1,k2[0]+1,k2[1]+1,bound,ch[0],bound,ch[0])) # fortran index ??
 
 
 #write bca file
 
-  with open(RPATH+basename+'.bca', 'wa') as f:
+    with open(RPATH+basename+'.bca', 'a') as f:
 
-    for ch in chunks:
+     for ch in chunks:
 
       for k,l in zip([ch[0],ch[-1]],le):
 
         if l == 'A' : label = k
 
-        plon=grd.x[k,0]
-        if plon < 0 : plon = plon + 360.
-        plat=grd.y[k,0]
+        plon=llon[k]
+        plat=llat[k]
         i=np.abs(lon-np.float(plon)).argmin()
         j=np.abs(lat-np.float(plat)).argmin()
 
         xx = lon[i-1:i+2]
         yy = lat[j-1:j+2]
-
 
         phv=np.zeros(ph.shape[-1])
         amv=np.zeros(amp.shape[-1])
@@ -99,8 +117,12 @@ for g,h in zip(s0,s1):
            fa=interpolate.RectBivariateSpline(xx,yy,zz,kx=2,ky=2)
            phv[m]= fa(plon,plat)
 
-        f.write('West{}{}\n'.format(label,l))
+        f.write('{}{}{}\n'.format(bound,label,l))
         for a,b,c in zip(tidal_c,amv,phv):
           f.write('{}         {:.7e}   {:.7e}\n'.format(a,b,c))
 
 
+getboundary('West',np.ones(lats.shape)*lons[0],lats)
+getboundary('East',np.ones(lats.shape)*lons[-1],lats)
+getboundary('North',lons,np.ones(lons.shape)*lats[-1])
+getboundary('South',lons,np.ones(lons.shape)*lats[0])
